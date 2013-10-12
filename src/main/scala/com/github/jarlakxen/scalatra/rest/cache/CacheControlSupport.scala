7f2de554ad224d15c42b4f88559c6f4d5a67e5dd
Facundo Viale <facundo.m.viale@gmail.com>
2013-10-12 18:45:26 -0300

    Move class to package, Add implicit

--- src/main/scala/com/github/jarlakxen/scalatra/rest/cache/CacheControlSupport.scala
+++ src/main/scala/com/github/jarlakxen/scalatra/rest/cache/CacheControlSupport.scala
@@ -1,33 +1,34 @@
-package com.github.jarlakxen.scalatra.rest
-
-import org.scalatra.ScalatraServlet
+package com.github.jarlakxen.scalatra.rest.cache
 import javax.servlet.http.{ HttpServletResponse, HttpServletRequest }
 import com.github.nscala_time.time.Imports._
+import org.joda.time.format.DateTimeFormatter
 
 trait CacheControlSupport {
-  implicit def request: HttpServletRequest
-  implicit def response: HttpServletResponse
+  implicit def request : HttpServletRequest
+  implicit def response : HttpServletResponse
+
+  implicit def CacheControlStrategy2Headers( cacheControlStrategy : CacheControlStrategy ) = cacheControlStrategy.headers
 
   val RFC1123_PATTERN = "EEE, dd MMM yyyyy HH:mm:ss z"
   val dateTimeFormat = DateTimeFormat.forPattern( RFC1123_PATTERN )
 
   sealed abstract class CacheControlStrategy {
-    def headers: Map[String, String]
+    def headers : Map[String, String]
   }
 
   case class NoCache() extends CacheControlStrategy {
     override def headers = Map( "Cache-Control" -> "no-store, no-cache, must-revalidate", "Pragma" -> "no-cache" )
   }
 
-  case class MaxAge( duration: Duration ) extends CacheControlStrategy {
+  case class MaxAge( duration : Duration ) extends CacheControlStrategy {
     override def headers = Map( "Cache-Control" -> ( "max-age=" + duration.seconds ) )
   }
 
-  case class Expires( expiration: DateTime ) extends CacheControlStrategy {
+  case class Expires( expiration : DateTime ) extends CacheControlStrategy {
     override def headers = Map( "Expires" -> dateTimeFormat.print( expiration ) )
   }
 
-  def cacheControl_=( cacheControl: CacheControlStrategy ) {
+  def cacheControl_=( cacheControl : CacheControlStrategy ) {
     for ( ( key, value ) <- cacheControl.headers ) response.addHeader( key, value )
   }
 
